87d15b449d9219a15af1954ef15dfcf0a4d70164
Kavandeep <kavan1@live.co.uk>
2022-07-06 15:32:26 +0100

    added function to capitalise first letter&comments

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 import pokemonArray from "./data/pokemon.js";
 
 const container = document.querySelector(".card-container");
-console.log(container)
+console.log(container);
 
+// function to capitalise first letter of name
+const capitaliseName = (name) => {
+    return name.charAt(0).toUpperCase() + name.slice(1);
+}
+
+// function that iterates through each object in the pokemon array and returns its details
 const addPokemon = () => {
     pokemonArray.forEach(pokemon => {
+
+        pokemon.name = capitaliseName(pokemon.name);
+        
         container.innerHTML += `
         <div class="card">
             <img src="${pokemon.sprite}">
@@ -14,8 +23,9 @@ const addPokemon = () => {
                 is a ${pokemon.types.join(" & ")} type pokemon.</div>
             </div>
         </div>`; 
-    })
+    });
 };
 
+// event listener that calls the addPokemon function when the page loads
 container.addEventListener("load", addPokemon());
 
